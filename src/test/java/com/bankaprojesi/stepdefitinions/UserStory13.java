c94dd46ca3396b143cd7e6abdf781298631a8234
Mehmetes012 <mehmetelmas012@gmail.com>
2021-03-31 15:40:15 -0500

    UserStory 13 eklendi

---
+++ src/test/java/com/bankaprojesi/stepdefitinions/UserStory13.java
@@ -0,0 +1,7 @@
+package com.bankaprojesi.stepdefitinions;
+
+public class UserStory13 {
+    public static void main(String[] args) {
+        System.out.println("Mehmet brachinden Merhabalar");
+    }
+}
